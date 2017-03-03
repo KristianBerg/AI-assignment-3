1634a4140aa0baa2850f3d8d8e5f861488e015b0
Mecha_Magpie <dat13egr@student.lu.se>
2017-03-03 10:09:45 +0100

    Added initialization of sensor probabilities

--- src/model/AwesomeLocalizer.scala
+++ src/model/AwesomeLocalizer.scala
@@ -1,11 +1,22 @@
 package model
 
 import control.EstimatorInterface
+import model.GridUtils._
+import breeze.linalg._
 
 /**
   * Created by ine13kbe on 28/02/17.
   */
 class AwesomeLocalizer extends EstimatorInterface{
+  val bot: BotSimulator = new BotSimulator()
+  val grid = Grid(bot.rows, bot.cols)
+  val sensorProb = for(reading <- grid) yield for(position <- grid) yield
+    if((reading.cdist(position)) == 2) 0.025
+    else if ((reading.cdist(position)) == 1) 0.05
+    else if (reading == position) 0.1
+    else 0
+  
+
   override def getNumRows: Int = ???
 
   override def getNumCols: Int = ???
